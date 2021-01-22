dfdcee3452c4c0bc6f37840e7d4f2ec7d528689d
mikecrbenton <mikecrbenton@hotmail.com>
2021-01-21 18:31:48 -0600

    build folder

---
+++ todo/build/precache-manifest.ef7e4468e70ba8c1d1f52148b20ef69b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbbf98e2fc299357bd5d8db7af8ad5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2488fe342e2a55490c8b",
+    "url": "/static/css/main.312e95f8.chunk.css"
+  },
+  {
+    "revision": "f4528fd6799c961ea47a",
+    "url": "/static/js/2.6dacfaef.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6dacfaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2488fe342e2a55490c8b",
+    "url": "/static/js/main.06e7135f.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
